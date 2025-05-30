4f85a9faa1b2626e22ef8dfac9a492034d58d40a
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2025-05-30 20:10:35 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { generateCalendar } from "./app"
 
 export default {
-  async fetch(request: Request) {
-    console.log("fetch", request.url)
+  async fetch(request: Request, env: any) {
     const url = new URL(request.url)
     try {
-      if (url.searchParams.get("calendar")) {
+      // Previously the index path was used with calendar param. This was
+      // moved to /calendar path to support matching the index.html file
+      // without having to run the worker.
+      if (url.pathname === "/calendar" || url.searchParams.get("calendar")) {
         const calendar = url.searchParams.get("calendar")!
         const isBase64 = Boolean(url.searchParams.get("base64"))
         const statuses = url.searchParams.get("status") || ""
+
         const ical = await generateCalendar(calendar, isBase64, statuses)
         return new Response(ical, {
           headers: {
@@ -17,7 +20,7 @@ export default {
           },
         })
       }
-      return new Response("Not Found 404", { status: 404 })
+      return env.ASSETS.fetch(request)
     } catch (error) {
       console.error("Could not generate home page or calendar", error)
       return new Response("<h1>500 Oops</h1>", { status: 500 })
